9c137b1d7d2370347c456f7963257622a7488a4a
Justin <wingpad@gmail.com>
2015-07-15 22:52:22 -0500

    Fixed stuff

--- src/com/lyte/objs/LyteObject.java
+++ src/com/lyte/objs/LyteObject.java
@@ -46,7 +46,7 @@ public class LyteObject extends HashMap<String, LyteValue> implements LyteValue<
 
   @Override
   public LyteValue getProperty(String property) {
-    if (hasProperty(property)) {
+    if (this.containsKey(property)) {
       return get(property);
     } else if (mGetters.containsKey(property)) {
       return mGetters.get(property).apply(new LyteContext(this));
@@ -59,8 +59,10 @@ public class LyteObject extends HashMap<String, LyteValue> implements LyteValue<
   public void setProperty(String property, LyteValue newValue) {
     if (mSetters.containsKey(property)) {
       mSetters.get(property).apply(new LyteContext(this, newValue));
-    } else {
+    } else if (!mGetters.containsKey(property)) {
       put(property, newValue);
+    } else {
+      throw new LyteError("Cannot set property " + property + " of object " + toString());
     }
   }
 
